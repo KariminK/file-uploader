97c6f9870b00896fa2066d1253f2a076c10e8808
KariminK <103779342+KariminK@users.noreply.github.com>
2025-01-11 21:05:58 +0100

    redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/views/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        cosmos:
+          "url('https://images.unsplash.com/photo-1716050866907-05f17aa6deda?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+    },
   },
   plugins: [],
 };
