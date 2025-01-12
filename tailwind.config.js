f0ea3b7efe13a011043bc29b53f34a5b614a1d3b
KariminK <103779342+KariminK@users.noreply.github.com>
2025-01-12 22:44:35 +0100

    exported navbar to partial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/views/*.ejs"],
+  content: ["./src/views/**/*.ejs"],
   theme: {
     extend: {
       backgroundImage: {
